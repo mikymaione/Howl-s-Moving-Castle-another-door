80dc216b621089b020f1c571298056d115cce426
Michele Maione <mikymaione@hotmail.it>
2018-12-03 09:10:30 +0100

    Updates

--- Resources/Levels/Radme/Cal/Blueprints/Castle/Blueprints.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Blueprints" tilewidth="50" tileheight="50" tilecount="480" columns="20">
- <image source="classic-blue.png" width="1000" height="1200"/>
-</tileset>
